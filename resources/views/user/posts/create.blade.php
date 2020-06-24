f59d8d2d54f47882321acbb7883f46393b5f7c65
romoon <romoon.h@gmail.com>
2020-06-25 04:10:00 +0900

    before heroku deploy

--- resources/views/user/posts/create.blade.php
+++ resources/views/user/posts/create.blade.php
@@ -6,7 +6,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>投稿の新規登録</h2>
+                <h2>新規投稿</h2>
                 <form action="{{ action('User\PostController@create') }}" method="post" enctype="multipart/form-data">
 
                     @if (count($errors) > 0)
